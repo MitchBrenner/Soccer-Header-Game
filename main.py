095e4bc309ee852441eef96fbec17c9b6b975b6c
MitchBrenner <mkbrenner3@wisc.edu>
2022-05-31 19:34:15 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import pygame
+
+# initialize pygame
+pygame.init()
+
+# create screen
+pygame.display.set_caption("Soccer Header")
+screen = pygame.display.set_mode((800, 600))
+
+while True:
+    # need to update screen
+    pygame.display.update()
